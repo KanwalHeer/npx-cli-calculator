65160c930bace2c49fb69cd2e6cc2f9633917227
KanwalHeer <160328670+KanwalHeer@users.noreply.github.com>
2024-03-20 02:59:58 +0500

    Add files via upload

---
+++ CLIcalculator.ts
@@ -0,0 +1,34 @@
+#!user/bin/env/node
+
+import inquirer from "inquirer"
+
+const answer = await inquirer.prompt([
+    {
+        name: "firstNumber",
+        type:"number",
+        message: "Enter your first Number"
+    },
+    {
+        name: "secondNumber",
+        type:"number",
+        message: "Enter your second Number"
+    },
+    {
+        name: "operator",
+        type:"list",
+        message: "choose your operator",
+        choices: ["+","-","*","/"]
+    }
+])
+
+if (answer.operator === "+") {
+    console.log(answer.firstNumber + answer.secondNumber)
+} else if(answer.operator === "-"){
+    console.log(answer.firstNumber - answer.secondNumber)
+} else if(answer.operator === "*"){
+    console.log(answer.firstNumber * answer.secondNumber)
+} else if(answer.operator === "/"){
+    console.log(answer.firstNumber / answer.secondNumber)
+}else{
+    console.log("Enter Valid Operator Please")
+}
